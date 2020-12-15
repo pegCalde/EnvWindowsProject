81fcff99f88af14255f5e22797c09a0197933c73
Peggy Calderon-Kayser <calderonpeg@gmail.com>
2020-12-15 10:15:56 +0100

    Ajoutez des fichiers projet.

---
+++ EnvWindowsProject/EnvWindowsProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EnvWindowsProject.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace EnvWindowsProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ EnvWindowsProject/EnvWindowsProject/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using EnvWindowsProject.Models;
+using EnvWindowsProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EnvWindowsProject.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
